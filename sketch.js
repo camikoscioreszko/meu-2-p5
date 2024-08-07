b99c8d2066b00bf640da336fa88186eb0ba35f16
camikoscioreszko <camile.koscioreszko@escola.pr.gov.br>
2024-08-07 14:45:58 -0300

    Add files via upload
    segundo projeto p5

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(1000, 1000);
+  background("black");
+}
+
+function draw() {
+ 
+stroke("white");
+fill ("orange");
+  
+  
+  
+  
+  if(mouseIsPressed) {
+   rect(mouseX, mouseY, 20, 20);
+    
+}    
+}
+
+  
